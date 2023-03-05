06f52d197b8be1ce3c15c0f3cfc511a2b595b094
adonsav <adonsav@gmail.com>
2023-03-05 17:05:25 +0200

    set admin proper email

--- migrations/20230302141857_add_user_to_database.postgres.up.sql
+++ migrations/20230302141857_add_user_to_database.postgres.up.sql
@@ -1,2 +1,2 @@
 INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
-    ('adon','sav','admin@test.com','$2a$12$8zrZ3FuksskihaXZ3rMovuUM0LjtQH418R8PWD42bewK4N/lSACWu',3,'2023-01-01 00:00:00','2023-01-01 00:00:00');
+    ('adon','sav','adonsav@gmail.com','$2a$12$8zrZ3FuksskihaXZ3rMovuUM0LjtQH418R8PWD42bewK4N/lSACWu',3,'2023-01-01 00:00:00','2023-01-01 00:00:00');
